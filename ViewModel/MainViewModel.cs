3a764e9801040aa8089d7a1a8000f1b8c3f43e15
AndersLemme <a.lemme@hotmail.com>
2025-03-10 23:01:41 +0100

    Added ViewModel folder and cs file for code behind

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    //example..
+    string text;
+    public string Text
+    {
+        get => text;
+        set
+        {
+            text = value;
+            OnPropertyChanged(nameof(text));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
